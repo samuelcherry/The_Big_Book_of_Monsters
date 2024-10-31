b41f1aa7cba65f9c112dc47322e8877e419607c8
Sam Cherry <76022735+samuelcherry@users.noreply.github.com>
2024-10-31 11:24:10 -0600

    fixed slot upgrade array, increase level also resets health.

--- Assets/Scripts/saveManager.cs
+++ Assets/Scripts/saveManager.cs
@@ -18,28 +18,33 @@ public class SaveManager : MonoBehaviour
     {
         float GoldAmt = enemyStats.GoldAmt;
         int Level = playerStats.level;
+
         int SlotOneLvl = slotUpgrades.slotOneLvl;
         int SlotTwoLvl = slotUpgrades.slotOneLvl;
         int SlotThreeLvl = slotUpgrades.slotOneLvl;
-        int SlotFourLvl = slotUpgrades.slotOneLvl;
+
         float Xp = playerStats.currentXp;
         float BaseXp = prestige.baseXP;
+
         float PrestigeMulti = prestige.prestigeMulti;
+        int AtkMetalCount = playerStats.atkMetalCount;
+        int DefMetalCount = playerStats.defMetalCount;
+        int HpMetalCount = playerStats.hpMetalCount;
 
         PlayerPrefs.SetFloat("GoldAmt", GoldAmt);
         PlayerPrefs.SetInt("Level", Level);
+
         PlayerPrefs.SetInt("SlotOneLvl", SlotOneLvl);
         PlayerPrefs.SetInt("SlotTwoLvl", SlotTwoLvl);
         PlayerPrefs.SetInt("SlotThreeLvl", SlotThreeLvl);
-        PlayerPrefs.SetInt("SlotFourLvl", SlotFourLvl);
+
         PlayerPrefs.SetFloat("Xp", Xp);
         PlayerPrefs.SetFloat("BaseXp", BaseXp);
         PlayerPrefs.SetFloat("PrestigeMulti", PrestigeMulti);
 
- 
-        PlayerPrefs.SetInt("AtkMetalCount", playerStats.atkMetalCount);
-        PlayerPrefs.SetInt("DefMetalCount", playerStats.defMetalCount);
-        PlayerPrefs.SetInt("HpMetalCount", playerStats.hpMetalCount);
+        PlayerPrefs.SetInt("AtkMetalCount", AtkMetalCount);
+        PlayerPrefs.SetInt("DefMetalCount", DefMetalCount);
+        PlayerPrefs.SetInt("HpMetalCount", HpMetalCount);
         
 
 
@@ -51,24 +56,30 @@ public class SaveManager : MonoBehaviour
     {
         float GoldAmt = PlayerPrefs.GetFloat("GoldAmt");
         int Level = PlayerPrefs.GetInt("Level");
+
         int SlotOneLvl = PlayerPrefs.GetInt("SlotOneLvl");
         int SlotTwoLvl = PlayerPrefs.GetInt("SlotTwoLvl");
         int SlotThreeLvl = PlayerPrefs.GetInt("SlotThreeLvl");
-        int SlotFourLvl = PlayerPrefs.GetInt("SlotFourLvl");
+
         float Xp = PlayerPrefs.GetFloat("Xp");
         float BaseXp = PlayerPrefs.GetFloat("BaseXp");
+
         float PrestigeMulti = PlayerPrefs.GetFloat("PrestigeMulti");
         int AtkMetalCount = PlayerPrefs.GetInt("AtkMetalCount");
         int DefMetalCount = PlayerPrefs.GetInt("DefMetalCount");
         int HpMetalCount = PlayerPrefs.GetInt("HpMetalCount");
 
         enemyStats.GoldAmt = GoldAmt;
+        
         playerStats.level = Level;
+
         slotUpgrades.slotOneLvl = SlotOneLvl;
         slotUpgrades.slotTwoLvl = SlotTwoLvl;
         slotUpgrades.slotThreeLvl = SlotThreeLvl;
+
         playerStats.currentXp = Xp;
         prestige.baseXP = BaseXp;
+
         prestige.prestigeMulti = PrestigeMulti;
         playerStats.atkMetalCount = AtkMetalCount;
         playerStats.defMetalCount = DefMetalCount;
